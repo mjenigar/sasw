df411ac337cc6905e2860b48dc36005e8259a6c2
Martin Jenigar <mjenigar6@gmail.com>
2021-12-03 01:33:43 +0100

    Model File + fixed train script

--- DataLoader.py
+++ DataLoader.py
@@ -29,8 +29,8 @@ class DataLoader():
         self.JoinDatasets()
         print('Cleaning dataset...')
         self.CleanData()
-        self.SplitData(0.6, 0.2, 0.2)
-        print("DATASET: train {} test {} val {}".format(0.6, 0.2, 0.2))
+        self.SplitData(0.7, 0.3)
+        print("DATASET: train {} test {}".format(0.7, 0.3))
         
         self.list_of_words = self.GetListOfWords()
         self.Tokenize()
@@ -41,14 +41,17 @@ class DataLoader():
         
         for dataset in self.datasets:
             for file in os.listdir("{}{}".format(self.root, dataset)):
+                if counter >= 64:
+                        break
+                    
                 if file[0] != "." and file != "submit.csv" and file != "test.csv":
                     full_path = "{}{}/{}".format(self.root, dataset, file)
                     print("Loading from: {}".format(full_path))
-                    
+
                     data = pd.read_csv(full_path)
                     for index, row in data.iterrows():
-                        # if counter > 20:
-                        #     break
+                        if counter > 64:
+                            break
                         
                         if dataset == "data1":
                             label = row.label
@@ -65,6 +68,8 @@ class DataLoader():
 
                         counter += 1
                     print("Loaded: {} rows\n".format(counter))
+            if counter >= 64:
+                break       
         print("Total loaded {} rows\n".format(len(self.dataset)))
 
         self.dataset = pd.DataFrame(self.dataset)
@@ -91,12 +96,11 @@ class DataLoader():
                 
         return list(set(list_of_words))
     
-    def SplitData(self, train_size, test_size, val_size):
-        if (train_size + test_size + val_size) != 1.0:
+    def SplitData(self, train_size, test_size):
+        if (train_size + test_size) != 1.0:
             return False
         
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.dataset.clean, self.dataset.label, test_size = test_size, shuffle=True)
-        self.x_train, self.x_val, self.y_train, self.y_val = train_test_split(self.x_train, self.y_train, test_size=val_size, shuffle=True)        
 
     def Tokenize(self, max_len=5000):
         # Create a tokenizer to tokenize the words and create sequences of tokenized words
